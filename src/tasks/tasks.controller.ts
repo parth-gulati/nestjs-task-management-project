c6d5d56275e45b7569f66609975fa5e2b5efd3f3
TP00214906 <gulati.parth98@gmail.com>
2022-03-20 15:11:14 +0530

    Started working on creating the controller for the project

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post } from '@nestjs/common';
+import { Task } from './task.model';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+    constructor(private tasksService: TasksService) {}
+
+    @Get()
+    getAllTasks(): Task[] {
+        return this.tasksService.getAllTasks()
+    }
+
+    @Post()
+    createTask(): Task { 
+        
+    }
+
+}
